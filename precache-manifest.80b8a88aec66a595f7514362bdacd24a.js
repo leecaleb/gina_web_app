08d7cf5c51f6bb64c2de4369d4f227a3dbaa2fee
Jane Wang <janewang@JanedeMacBook-Air.local>
2020-06-01 12:34:05 +0800

    Updates

---
+++ precache-manifest.80b8a88aec66a595f7514362bdacd24a.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = [
+  {
+    "revision": "112d2762087c0d97058c",
+    "url": "/gina_web_app/static/js/app.3db0bb38.chunk.js"
+  },
+  {
+    "revision": "42ef4740eb5dc25b6be4",
+    "url": "/gina_web_app/static/js/runtime~app.ad331959.js"
+  },
+  {
+    "revision": "40524a423a0ccc192498",
+    "url": "/gina_web_app/static/js/2.30d16b42.chunk.js"
+  },
+  {
+    "revision": "f8c9cedd86583d42509129faf388066d",
+    "url": "/gina_web_app/static/media/icon-thumbnail.f8c9cedd.png"
+  },
+  {
+    "revision": "0693cca6b81f70de8b8f4b84f86919e4",
+    "url": "/gina_web_app/static/media/icon-teacher-thumbnail.0693cca6.png"
+  },
+  {
+    "revision": "fee19d5834568fe23ce905b4bce0ba3c",
+    "url": "/gina_web_app/static/media/app_logo.fee19d58.png"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/gina_web_app/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/gina_web_app/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/gina_web_app/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/gina_web_app/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/gina_web_app/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/gina_web_app/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/gina_web_app/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/gina_web_app/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/gina_web_app/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/gina_web_app/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/gina_web_app/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/gina_web_app/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "add46308d75b74f28bcd2682ed594479",
+    "url": "/gina_web_app/static/media/icon-checked-box.add46308.png"
+  },
+  {
+    "revision": "ea8d7086b8f43f643517cd559a0c3001",
+    "url": "/gina_web_app/static/media/icon-medication-powder.ea8d7086.png"
+  },
+  {
+    "revision": "a7ee612bcc3f09a0ab4ee85f4c2120c0",
+    "url": "/gina_web_app/static/media/icon-medication-syrup.a7ee612b.png"
+  },
+  {
+    "revision": "12878684b4ad0c861de24bf63fac68d6",
+    "url": "/gina_web_app/static/media/icon-medication-gel.12878684.png"
+  },
+  {
+    "revision": "b0c168c9affbcefc83ad2f1558ef7caa",
+    "url": "/gina_web_app/static/media/icon-medication-pill.b0c168c9.png"
+  },
+  {
+    "revision": "8e4dfdb5ce0ba232333ab98e49bf7e1b",
+    "url": "/gina_web_app/static/media/icon-wellness.8e4dfdb5.png"
+  },
+  {
+    "revision": "02c27d2cade25cc47e6c8bee53e6e8dd",
+    "url": "/gina_web_app/static/media/icon-milk.02c27d2c.png"
+  },
+  {
+    "revision": "236e3240e652b98f814029d2ce067be3",
+    "url": "/gina_web_app/static/media/icon-diaper.236e3240.png"
+  },
+  {
+    "revision": "a7fc62df357d3d180fa9c6243762c4f4",
+    "url": "/gina_web_app/static/media/icon-babies.a7fc62df.png"
+  },
+  {
+    "revision": "83ee9de4775b7688d1212478d06de792",
+    "url": "/gina_web_app/static/media/icon-checked.83ee9de4.png"
+  },
+  {
+    "revision": "9cb15f8581847cd41ea35eb74b577503",
+    "url": "/gina_web_app/static/media/icon-morningreminder.9cb15f85.png"
+  },
+  {
+    "revision": "e319f13c68960ddcab02773352c8f43c",
+    "url": "/gina_web_app/static/media/icon-plus.e319f13c.png"
+  },
+  {
+    "revision": "128442cf604f3177bc4d92abd1fc0eba",
+    "url": "/gina_web_app/static/media/icon-plus-green.128442cf.png"
+  },
+  {
+    "revision": "4fa4cf9417d1d85a70fd65d5a98e9153",
+    "url": "/gina_web_app/static/media/icon-plus-red.4fa4cf94.png"
+  },
+  {
+    "revision": "54a91b0619ccf9373d525109268219dc",
+    "url": "/gina_web_app/./fonts/Roboto.ttf"
+  },
+  {
+    "revision": "99fc0816a09395454061301fefa42bf1",
+    "url": "/gina_web_app/./fonts/Roboto_medium.ttf"
+  },
+  {
+    "revision": "0a3ec9350d6d25cea5b9433d61906f2e",
+    "url": "/gina_web_app/static/media/app-splash.0a3ec935.gif"
+  },
+  {
+    "revision": "9029a33d4f4fd27d78809364ba8b3ca0",
+    "url": "/gina_web_app/.expo/settings.json"
+  },
+  {
+    "revision": "635fb800c8c803b076ab4ce284207556",
+    "url": "/gina_web_app/serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "/gina_web_app/expo-service-worker.js"
+  },
+  {
+    "revision": "c5e28cce64ee8171a174789f78b968fb",
+    "url": "/gina_web_app/favicon-16.png"
+  },
+  {
+    "revision": "905cb0ed1d404283e330615037f2b4c1",
+    "url": "/gina_web_app/favicon-32.png"
+  },
+  {
+    "revision": "22cba4804bdff3434571b7f13cfaed80",
+    "url": "/gina_web_app/favicon.ico"
+  },
+  {
+    "revision": "253a526c8b9c31cfad96c73f4cd428db",
+    "url": "/gina_web_app/index.html"
+  },
+  {
+    "revision": "a81b430a03e10d7d1e3fae953c650a67",
+    "url": "/gina_web_app/manifest.json"
+  },
+  {
+    "revision": "9b0fc841417ab1178f006033bb08172e",
+    "url": "/gina_web_app/register-service-worker.js"
+  }
+];
