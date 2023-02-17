425b694dbb4546ded9066c0e220e3a979cbc5092
caleb <caleb7947@gmail.com>
2023-02-16 19:24:06 -0500

    hi

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function(env, argv) {
-  const config = await createExpoWebpackConfigAsync({ ...env, offline: false }, argv);
+  const config = await createExpoWebpackConfigAsync({ ...env }, argv);
   // Customize the config before returning it.
   return config;
 };
